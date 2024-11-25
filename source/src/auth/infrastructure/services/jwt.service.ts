e0708bdeb150fa31a7a5f2b2362a5db0c7f5ce14
suojae <suojae.developer@gmail.com>
2024-11-25 12:28:27 +0900

    feat: jwt-service 내부함수 추가

--- source/src/auth/infrastructure/services/jwt.service.ts
+++ source/src/auth/infrastructure/services/jwt.service.ts
@@ -1,5 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService as NestJwtService } from '@nestjs/jwt';
 
+@Injectable()
 export class JwtService {
+  constructor(private readonly jwtService: NestJwtService) {}
 
+  generateAccessToken(payload: any): string {
+    return this.jwtService.sign(payload, { expiresIn: '1h' });
+  }
+
+  generateRefreshToken(payload: any): string {
+    return this.jwtService.sign(payload, { expiresIn: '30d' });
+  }
+
+  verifyAccessToken(token: string): any {
+    return this.jwtService.verify(token);
+  }
+
+  cerifyRefreshToken(token: string): any {
+    return this.jwtService.verify(token);
+  }
 }
