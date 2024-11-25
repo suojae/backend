2108355dbdc3168b20e8f0bfb36603ccd9c914fe
suojae <suojae.developer@gmail.com>
2024-11-25 22:43:13 +0900

    feat: bcrypt-service 추가

--- source/src/auth/infrastructure/services/bcryptService.ts
+++ source/src/auth/infrastructure/services/bcryptService.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class BcryptService {}
+export class BcryptService {
+  private readonly saltRounds = 10;
+
+  /**
+   * 비밀번호를 해싱합니다
+   * @param password 원본 비밀번호
+   * @returns 해싱된 비밀번호
+   */
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, this.saltRounds);
+  }
+
+  /**
+   * 원본 비밀번호와 해싱된 비밀번호를 비교합니다
+   * @param password 원본 비밀번호
+   * @param hashedPassword 해싱된 비밀번호
+   * @returns 비밀번호가 일치여부 bool타입 반환
+   */
+  async comparePassword(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+}
