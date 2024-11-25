69193b33d0e4e84d3a69f68bb1020af8f69f229b
suojae <suojae.developer@gmail.com>
2024-11-25 22:43:32 +0900

    test: bcrypt-service 단위테스트 추가

---
+++ source/test/infrastructure/services/bcrypt.service.spec.ts
@@ -0,0 +1,50 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { BcryptService } from '../../../src/auth/infrastructure/services/bcryptService';
+import * as bcrypt from 'bcrypt';
+
+describe('BcryptService', () => {
+  let bcryptService: BcryptService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [BcryptService],
+    }).compile();
+
+    bcryptService = module.get<BcryptService>(BcryptService);
+  });
+
+  describe('hashPassword', () => {
+    it('비밀번호가 주어지고, 해싱 요청을 했을 때, 해싱된 비밀번호를 반환한다', async () => {
+      // Given
+      const password = 'sample_password';
+      const hashedPasswordMock = 'hashedPassword';
+      jest.spyOn(bcrypt, 'hash').mockResolvedValue(hashedPasswordMock);
+
+      // When
+      const result = await bcryptService.hashPassword(password);
+
+      // Then
+      expect(result).toBe(hashedPasswordMock);
+      expect(bcrypt.hash).toHaveBeenCalledWith(password, 10); // saltrounds 확인
+    });
+  });
+
+  describe('comparePassword', () => {
+    it('원본 비밀번호와 해싱된 비밀번호가 주어지고, 비교를 했을 때, 비밀번호가 일치하면 true를 반환한다', async () => {
+      // Given
+      const password = 'secure_password';
+      const hashedPassword = 'hashed_password';
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(true);
+
+      // When
+      const result = await bcryptService.comparePassword(
+        password,
+        hashedPassword,
+      );
+
+      // Then
+      expect(result).toBe(true);
+      expect(bcrypt.compare).toHaveBeenCalledWith(password, hashedPassword);
+    });
+  });
+});
