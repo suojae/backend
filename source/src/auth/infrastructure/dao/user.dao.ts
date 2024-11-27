e7bf08ce9c53e784fc83c873f5ba317084f52ad7
suojae <suojae.developer@gmail.com>
2024-11-27 22:36:49 +0900

    feat: erd설계따라 user-dao 구현

--- source/src/auth/infrastructure/dao/user.dao.ts
+++ source/src/auth/infrastructure/dao/user.dao.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
-@Injectable()
-export class UserDAO {}
+@Entity('User')
+export class UserDAO {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  nickname: string;
+
+  @Column({ type: 'enum', enum: ['rabbit', 'dog', 'hamster', 'cat'] })
+  character_id: 'rabbit' | 'dog' | 'hamster' | 'cat';
+
+  @Column({ type: 'int', default: 0 })
+  report_count: number;
+
+  @Column({ type: 'enum', enum: ['google', 'kakao', 'apple'] })
+  social_provider: 'google' | 'kakao' | 'apple';
+
+  @Column({ type: 'varchar', length: 255 })
+  social_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
