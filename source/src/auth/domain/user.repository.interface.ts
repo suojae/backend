d54d54ef345712e7329e05697a8eee638fa20768
suojae <suojae.developer@gmail.com>
2024-11-29 16:09:11 +0900

    feat: user-repository-interface 추가

--- source/src/auth/domain/user.repository.interface.ts
+++ source/src/auth/domain/user.repository.interface.ts
@@ -1,5 +1,13 @@
 import { UserEntity } from './user.entity';
+
+export const IUserRepository = Symbol('IUserRepository');
+
 export interface IUserRepository {
-  findById(id: string): Promise<UserEntity | null>;
-  save(user: UserEntity): Promise<UserEntity>;
+  save(userEntity: UserEntity): Promise<UserEntity>;
+
+  findById(uuid: string): Promise<UserEntity | null>;
+
+  update(uuid: string, partialUser: Partial<UserEntity>): Promise<void>;
+
+  delete(uuid: string): Promise<void>;
 }
