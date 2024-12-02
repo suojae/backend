618ee97765e77577466025e447f7c0463dc8db2b
suojae <suojae.developer@gmail.com>
2024-12-02 22:07:41 +0900

    fix: redis-config 버전 수정 및 store추가

--- source/src/config/redis.config.ts
+++ source/src/config/redis.config.ts
@@ -1,4 +1,9 @@
-export const redisConfig = {
-  host: process.env.REDIS_HOST,
-  port: +process.env.REDIS_PORT,
-};
+import * as redisStore from 'cache-manager-redis-store';
+import { ConfigService } from '@nestjs/config';
+
+export const redisConfig = (configService: ConfigService) => ({
+  store: redisStore as undefined as string,
+  host: configService.get<string>('REDIS_HOST') || 'localhost',
+  port: configService.get<number>('REDIS_PORT') || 6379,
+  ttl: Math.max(configService.get<number>('CACHE_TTL') || 600, 1), // TTL 최소값 보장
+});
