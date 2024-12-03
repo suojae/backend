7b9f40e6e617e516398e226d9e8ebb5f6805ab6d
suojae <suojae.developer@gmail.com>
2024-12-03 15:14:24 +0900

    fix: redis 사용 패키지 변경

--- source/src/config/redis.config.ts
+++ source/src/config/redis.config.ts
@@ -1,9 +1,10 @@
-import * as redisStore from 'cache-manager-redis-store';
 import { ConfigService } from '@nestjs/config';
 
 export const redisConfig = (configService: ConfigService) => ({
-  store: redisStore as undefined as string,
-  host: configService.get<string>('REDIS_HOST') || 'localhost',
-  port: configService.get<number>('REDIS_PORT') || 6379,
-  ttl: Math.max(configService.get<number>('CACHE_TTL') || 600, 1), // TTL 최소값 보장
+  store: 'redis',
+  socket: {
+    host: configService.get<string>('REDIS_HOST', 'localhost'),
+    port: configService.get<number>('REDIS_PORT', 6379),
+  },
+  ttl: Math.max(configService.get<number>('CACHE_TTL', 600), 1), // TTL 최소값 보장
 });
