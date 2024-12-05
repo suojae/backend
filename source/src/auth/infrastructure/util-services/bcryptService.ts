896195e11d0b1e42d7b54570ac22e4cb88197f72
suojae <suojae.developer@gmail.com>
2024-12-05 15:29:21 +0900

    feat: bcyprt-service 에러처리 로직 추가

--- source/src/auth/infrastructure/util-services/bcryptService.ts
+++ source/src/auth/infrastructure/util-services/bcryptService.ts
@@ -1,29 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, InternalServerErrorException} from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class BcryptService {
-  private readonly saltRounds = 10;
+    private readonly saltRounds = 10;
 
-  /**
-   * 비밀번호를 해싱합니다
-   * @param password 원본 비밀번호
-   * @returns 해싱된 비밀번호
-   */
-  async hashPassword(password: string): Promise<string> {
-    return bcrypt.hash(password, this.saltRounds);
-  }
+    /**
+     * 비밀번호를 해싱합니다
+     * @param password 원본 비밀번호
+     * @returns 해싱된 비밀번호
+     * @throws InternalServerErrorException 해싱 중 오류 발생 시
+     */
+    async hashPassword(password: string): Promise<string> {
+        try {
+            return await bcrypt.hash(password, this.saltRounds);
+        } catch (err) {
+            throw new InternalServerErrorException(
+                '비밀번호 해싱 중 오류가 발생했습니다.',
+            );
+        }
+    }
 
-  /**
-   * 원본 비밀번호와 해싱된 비밀번호를 비교합니다
-   * @param password 원본 비밀번호
-   * @param hashedPassword 해싱된 비밀번호
-   * @returns 비밀번호가 일치여부 bool타입 반환
-   */
-  async comparePassword(
-    password: string,
-    hashedPassword: string,
-  ): Promise<boolean> {
-    return bcrypt.compare(password, hashedPassword);
-  }
+    /**
+     * 원본 비밀번호와 해싱된 비밀번호를 비교합니다
+     * @param password 원본 비밀번호
+     * @param hashedPassword 해싱된 비밀번호
+     * @returns 비밀번호가 일치여부 (boolean)
+     * @throws InternalServerErrorException 비교 중 오류 발생 시
+     */
+    async comparePassword(
+        password: string,
+        hashedPassword: string,
+    ): Promise<boolean> {
+        try {
+            return await bcrypt.compare(password, hashedPassword);
+        } catch (err) {
+            throw new InternalServerErrorException(
+                '비밀번호 비교 중 오류가 발생했습니다.',
+            );
+        }
+    }
 }
